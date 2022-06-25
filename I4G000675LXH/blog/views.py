e21719b94046c34a46b335aa565f10626e878cb1
Biniyam Melaku <BiniyamMelaku2@gmail.com>
2022-06-25 11:15:29 +0300

    Django CRUD blog posts

--- I4G000675LXH/blog/views.py
+++ I4G000675LXH/blog/views.py
@@ -9,28 +9,28 @@ from django.urls import reverse_lazy
 class PostListView(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by('-publish')
-    template_name = 'post_list.html'
+    template_name = 'blog/post_list.html'
 
 class PostCreateView(generic.CreateView):
     model = Post
     fields = "__all__"
     success_url  = reverse_lazy("blog:all")
-    template_name = 'post_create.html'
+    template_name = 'blog/post_form.html'
 
 class PostDetailView(generic.DetailView):
     model = Post
-    template_name = 'post_detail.html'
+    template_name = 'blog/post_detail.html'
 
 
 class PostUpdateView(generic.UpdateView):
     model = Post
     fields = "__all__"
     success_url  = reverse_lazy("blog:all")
-    template_name = 'post_update.html'
+    template_name = 'blog/post_update.html'
 
 
 class PostDeleteView(generic.UpdateView):
     model = Post
     fields = "__all__"
     success_url  = reverse_lazy("blog:all")
-    template_name = 'post_delete.html'
+    template_name = 'blog/post_delete.html'
